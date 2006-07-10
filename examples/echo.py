1bba895d900d2a50c94947aee6f9929aaebc76d3
liquidx <liquidx@47920889-6d0e-0410-b696-d5024e80b239>
2006-07-10 00:09:57 +0000

    fixed example and made echobot
    git-svn-id: http://svn.berlios.de/svnroot/repos/python-silc/trunk@12 47920889-6d0e-0410-b696-d5024e80b239

---
+++ examples/echo.py
@@ -0,0 +1,47 @@
+import time
+import silc
+
+class EchoClient(silc.SilcClient):
+
+      def channel_message(self, sender, channel, flags, message):
+          print message
+          self.send_channel_message(channel, message)
+
+      def private_message(self, sender, flags, message):
+          print message
+          self.send_private_message(sender, message)
+
+      def connected(self):
+          print "* Connected"
+          self.command_call("JOIN #cam")
+
+      def disconnected(self, msg):
+          print "* Disconnected: %s" % msg
+
+      # catch responses to commands
+
+      def command_reply_join(self, channel, name, topic, hmac, x, y,
+          users):
+
+          print "* Joined channel %s" % name
+          self.send_channel_message(channel, "Hello!")
+           
+      # catch async notifications from the server
+
+      def notify_join(self, user, channel):
+          print "* A user named %s has joined the channel %s" % \
+                (user.username, channel.channel_name)
+          self.send_channel_message(channel, "Hello, %s" %
+          user.username)
+
+if __name__ == "__main__":
+   keys = silc.create_key_pair("silc.pub", "silc.prv", passphrase = "")
+   client = EchoClient(keys, "echobot", "echobot", "Echo Bot")
+   client.connect_to_server("silc.example.com")
+   
+   while True:
+       try:
+           client.run_one()
+           time.sleep(0.2)
+       except KeyboardInterrupt:
+           break
